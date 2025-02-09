d7b5074dbf5d1f398ef39426587a52f31974373b
eagyemang21 <elijahagyemang9@gmail.com>
2025-02-08 21:09:35 -0500

    Added psuedocode and box stylings

---
+++ functionality.js
@@ -0,0 +1,11 @@
+/*
+-First, create 4 boxes that are different colors in a row.
+-Next, create a start button
+    -once that button is pressed, it will turn into a stop button
+-when game starts, the computer (at random), must start by putting in the first pattern
+-if player chooses the right one, it continues, where the number of computer patterns increases by one, and the user repeats
+-so on
+-8 times is the maximum!
+-If player loses, there is a browser pop-up that says "You lost the game." and will return to initial state.
+-If player wins, there's a browser pop-up that says "Congratulations! You win!" and will return to initial state.
+*/ 
