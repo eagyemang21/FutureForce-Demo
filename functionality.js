ef2936a6533abca2a4f9edc35ff1a6f81415f33b
eagyemang21 <elijahagyemang9@gmail.com>
2025-02-08 22:21:46 -0500

    Initiating setup for moves

--- functionality.js
+++ functionality.js
@@ -1,5 +1,5 @@
 /*
--First, create 4 boxes that are different colors in a row.
+
 -Next, create a start button
     -once that button is pressed, it will turn into a stop button
 -when game starts, the computer (at random), must start by putting in the first pattern
@@ -9,3 +9,27 @@
 -If player loses, there is a browser pop-up that says "You lost the game." and will return to initial state.
 -If player wins, there's a browser pop-up that says "Congratulations! You win!" and will return to initial state.
 */ 
+//initiate boxes with colors
+let boxColors = ["red", "purple", "green", "yellow"]
+//next, I want to store the auto-generated computer patterns and player moves in arrays
+let computerPatterns = []
+let playerMoves = []
+
+//initiate level
+let level = 0
+
+//player's turn must be false when computer turns occur, will turn true when computer
+let playerTurn = false
+
+function gameStart(){
+    //call computerpatterns, level, playermoves, and subsequent rounds to either be manipulated
+    computerPatterns = [];
+    level = 0;
+    playerMoves = [];
+    subsequentRounds();
+
+}
+
+function subsequentRounds(){
+
+}
